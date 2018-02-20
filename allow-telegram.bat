e56666ac21929812fbb21351ecdd3bd21653d5c0
Larry Bohn <larrybohn@users.noreply.github.com>
2018-02-20 14:26:48 +0300

    added gateway variable to the batch file

--- allow-telegram.bat
+++ allow-telegram.bat
@@ -1,5 +1,6 @@
-route add -p 149.154.164.0/22 192.168.10.1
-route add -p 149.154.160.0/20 192.168.10.1
-route add -p 91.108.4.0/22 192.168.10.1
-route add -p 91.108.56.0/22 192.168.10.1
-route add -p 91.108.8.0/22 192.168.10.1
+set /p gateway="Enter Gateway IP: "
+echo route add -p 149.154.164.0/22 %gateway%
+echo route add -p 149.154.160.0/20 %gateway%
+echo route add -p 91.108.4.0/22 %gateway%
+echo route add -p 91.108.56.0/22 %gateway%
+echo route add -p 91.108.8.0/22 %gateway%
